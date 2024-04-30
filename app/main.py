1b65117cb0652f3b2b9646ef570770d538e5b7f1
pmirandaf <pablo.miranda.oliveira@gmail.com>
2024-04-30 09:52:41 -0300

    fix: Corrigindo problema no deploy

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.routers.routes import router
+from app.middleware.middleware import log_requests
 
 
 app = FastAPI()
 app.include_router(router)
+app.middleware("http")(log_requests)
